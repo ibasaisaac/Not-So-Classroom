7e234569b5762e9f494c970b04e974ab548468d3
tirednemo <ummetasnim@iut-dhaka.edu>
2022-11-03 02:56:42 +0600

    ignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/home";
 import Verification from "./components/verification";
 import Login from "./components/login";
-import Navbar from "./components/navbar";
+import Navbar from "./components/hamburger";
 import Register from "./components/register";
  
 function App() {
