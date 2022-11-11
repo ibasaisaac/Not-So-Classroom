72acec71165bb0c82794c6fb88c8c029f102a5c8
tirednemo <ummetasnim@iut-dhaka.edu>
2022-11-11 11:03:15 +0600

    offcanvas hamburger in header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -2,9 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import jwt_decode from "jwt-decode";
-import { ToastContainer, toast } from 'react-toastify';
 
-import Hamburger from './hamburger'
 import '../static/header.css';
 import logo from '../static/pencil.svg';
 
@@ -52,7 +50,7 @@ const Header = () => {
         return Promise.reject(error);
     });
 
-    
+
     const Logout = async () => {
         try {
             await axios.delete('http://localhost:5000/logout');
@@ -63,22 +61,113 @@ const Header = () => {
     }
 
 
+    const homeButton = async () => {
+        history.push("./home");
+    }
+    const profileButton = async () => {
+        history.push("./profile");
+    }
+    const groupButton = async () => {
+        history.push("./group");
+    }
+    const clubButton = async () => {
+        history.push('./club');
+    }
+    const shopButton = async () => {
+        history.push("./shop");
+    }
+    const supportButton = async () => {
+        history.push("./support");
+    }
+
     return (
-        <div >
-            <div className='outer marker' >
-
-                <h3 className="innerMiddle"> <img src={logo} width="24" height="24" alt="logo" />Not So Classroom</h3>
-
-                <div className='innerRight'>
-                    <label> {username}, &ensp; </label>
-                    <button onClick={Logout} style={{ backgroundColor: 'transparent', border: 'none' }}>
-                        <i className="fa fa-right-from-bracket fa-xl"></i>
+        <div className='sticky-top marker' style={{ backgroundColor: 'var(--crystal)' }}>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <button type="button" style={{ backgroundColor: 'transparent', border: 'none' }} data-bs-toggle="offcanvas" data-bs-target="#offcanvasSidenav">
+                        <i className="fa fa-solid fa-bars fa-xl" ></i>
                     </button>
+
+                    <a className="navbar-brand" onClick={homeButton}><img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />Not So Classroom</a>
+                    <div>
+                        <label> {username}, &ensp; </label>
+                        <button onClick={Logout} style={{ backgroundColor: 'transparent', border: 'none' }}>
+                            <i className="fa fa-right-from-bracket fa-xl"></i>
+                        </button>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="offcanvas offcanvas-start" style={{ maxWidth: '25%', backgroundColor: 'var(--crystal)' }} data-bs-scroll="true" tabIndex="-1" id="offcanvasSidenav">
+
+                <div className="offcanvas-header p-4" style={{ maxHeight: '45px'}}>
+                    <h5 className="offcanvas-title">Ahoy there, {username}!</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
+                </div>
+
+                <div className="offcanvas-body" >
+                    <div className="offcanvas-body comfortaa" >
+                        <ul className="navbar-nav gap-2">
+                            <li>
+                                <button type="button" className="btn btn-light btn-block text-center" data-bs-dismiss="offcanvas" onClick={homeButton}>
+                                    <a>Home</a>
+                                </button>
+                            </li>
+                            <li >
+                                <button type="button" className="btn btn-light btn-block text-center" data-bs-dismiss="offcanvas" onClick={profileButton}>
+                                    <a>Profile</a>
+                                </button>
+                            </li>
+                            <li>
+                                <button type="button" className="btn btn-light btn-block text-center" data-bs-dismiss="offcanvas" onClick={groupButton}>
+                                    <a>Group</a>
+                                </button>
+                            </li>
+                            <li>
+                                <button type="button" className="btn btn-light btn-block text-center" data-bs-dismiss="offcanvas" onClick={clubButton}>
+                                    <a>Clubs</a>
+                                </button>
+                            </li>
+                            <li >
+                                <button type="button" className="btn btn-light btn-block text-center" data-bs-dismiss="offcanvas" onClick={shopButton}>
+                                    <a>Shop</a>
+                                </button>
+                            </li>
+                            <li>
+                                <button type="button" className="btn btn-light btn-block text-center" data-bs-dismiss="offcanvas" onClick={supportButton}>
+                                    <a >Support</a>
+                                </button>
+                            </li>
+
+                            < div className="penguin" >
+                                <div className="penguin-head">
+                                    <div className="face left"></div>
+                                    <div className="face right"></div>
+                                    <div className="chin"></div>
+                                    <div className="eye left">
+                                        <div className="eye-lid"></div>
+                                    </div>
+                                    <div className="eye right">
+                                        <div className="eye-lid"></div>
+                                    </div>
+                                    <div className="blush left"></div>
+                                    <div className="blush right"></div>
+                                    <div className="beak top"></div>
+                                    <div className="beak bottom"></div>
+                                </div>
+                                <div className="shirt">
+                                </div>
+                                <div className="penguin-body">
+                                    <div className="arm left"></div>
+                                    <div className="arm right"></div>
+                                    <div className="foot left"></div>
+                                    <div className="foot right"></div>
+                                </div>
+                            </div >
+                        </ul>
+                    </div>
                 </div>
             </div>
-
-            {/* <ToastContainer position="top-right" progressClassName="toastProgress" /> */}
-            <Hamburger/>
         </div>
     )
 }
